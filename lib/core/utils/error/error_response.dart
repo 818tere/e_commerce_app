a9d6f69de1aca9cfa3de37411b467fa4b67ccf68
818tere <poulee333@gmail.com>
2024-01-05 17:25:09 +0900

    DomainLayer - base usecase 추가

---
+++ lib/core/utils/error/error_response.dart
@@ -0,0 +1,9 @@
+class ErrorResponse {
+  final String? status;
+  final String? message;
+  final String? code;
+
+  ErrorResponse(
+      {this.status = 'SUCCESS', this.message = '', this.code = '200'});
+}
+// usecase의 error response
